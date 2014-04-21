a08a19ed2abe7bd14b50c654a8549378ebd15fbb
liuyf <liuyf@joy.cn>
2014-04-21 19:02:09 +0800

    add full_title helper method for ApplicationController

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "毕业生就业信息管理系统"
+
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
