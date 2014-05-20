f7813544a2732a4f36a51a7a130a00b4eae8ba2b
liuyf <liuyf@joy.cn>
2014-05-20 13:51:59 +0800

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Admin.create(:email => "admin@ntu.cn", :password => '11111111', :password_confirmation => "11111111")
+Admin.create(:email => "admin@ntu.cn", :password => '11111111', :password_confirmation => "11111111")
 
 Gender.create(:name => '男')
 Gender.create(:name => '女')
@@ -15,3 +15,15 @@ Grade.create(:name => '大一')
 Grade.create(:name => '大二')
 Grade.create(:name => '大三')
 Grade.create(:name => '大四')
+
+Subject.create(:name => '计算机学院')
+
+Major.create(:name => '软件工程')
+
+CompanyType.create(:name => '民营企业')
+
+CompanyScale.create(:name => '50 人以下')
+
+PostType.create(:name => 'IT')
+
+PostSubType.create(:name => '计算机硬件')
